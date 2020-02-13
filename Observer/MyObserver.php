a30e490404d7664d990df9f375e2eed195dfd376
Stas Puga <stas.puga@transoftgroup.com>
2020-02-13 13:38:39 +0200

    Module Action

--- Observer/MyObserver.php
+++ Observer/MyObserver.php
@@ -3,9 +3,36 @@ namespace Puga\Action\Observer;
 
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\Data\Tree\Node;
+use Magento\Framework\UrlInterface;
+use \Magento\Store\Model\StoreManagerInterface;
 
 class MyObserver implements ObserverInterface
 {
+
+    /**
+     * @var StoreManagerInterface
+     */
+    protected $_storeManager;
+
+    /**
+     * @var UrlInterface
+     */
+    protected $_urlInterface;
+
+    /**
+     * MyObserver constructor.
+     * @param \Magento\Store\Model\StoreManagerInterface $_storeManager
+     * @param \Magento\Framework\UrlInterface $urlInterface
+     */
+    public function __construct(
+        StoreManagerInterface $_storeManager,
+        UrlInterface $urlInterface
+    )
+    {
+        $this->_storeManager = $_storeManager;
+        $this->_urlInterface = $urlInterface;
+    }
+
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
         /* @var $menu \Magento\Framework\Data\Tree\Node */
@@ -13,13 +40,15 @@ class MyObserver implements ObserverInterface
         $tree = $menu->getTree();
         $node = new Node(
           [
-              'name' => 'Action',
+              'name' => __('Action'),
               'id' => 'puga_action',
-              'url' => '/puga_action'
+              'url' => $this->_storeManager->getStore()->getBaseUrl(UrlInterface::URL_TYPE_WEB) . 'puga_action/index/action/'
           ],
             'id',
-            $tree
+            $tree,
+            $menu
+
         );
-        return $node;
+        return $menu->addChild($node);
     }
 }
